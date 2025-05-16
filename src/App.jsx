760a09c8064bd4eb71048fd2f08ed65dd05340ca
arpitsharma-7886 <arpitsharma7886@gmail.com>
2025-05-16 13:26:57 +0530

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/Header'
-import MainTemplate from './components/MainTemplate'
-import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import Home from './pages/Home'
 import LoginPage from './pages/LoginSignUp'
 import OtpVerification from './pages/Otp'
 import ProductDetails from './pages/ProductDetails'
@@ -13,11 +10,12 @@ import CheckoutPage from './pages/CheckoutPage'
 import OrderSuccessPage from './pages/OrderSuccessPage'
 import CategoryProducts from './pages/CategoryProducts'
 import WishlistPage from './pages/WishlistPage'
-import { Toaster } from 'react-hot-toast'
+import OrdersPage from './pages/OrdersPage'
+import AccountPage from './pages/AccountPage'
+import AddressesPage from './pages/AddressesPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-
-  
   return (
     <BrowserRouter>
       <Toaster position="top-center" />
@@ -26,11 +24,18 @@ function App() {
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/otp' element={<OtpVerification/>} />
         <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/checkout/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/sub-category/:name/:id" element={<CategoryProducts />} />
-        <Route path="/order-success" element={<OrderSuccessPage />} />
-        <Route path="/wishlist" element={<WishlistPage />} />
+        
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/checkout/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/order-success" element={<OrderSuccessPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/addresses" element={<AddressesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
